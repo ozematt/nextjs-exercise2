e8b8306e7fcaa13a50b0458ec92419275fef2ce6
Mateusz Obirek <neutralnie7@gmail.com>
2025-02-25 13:29:40 +0100

    added logic

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,15 +11,21 @@ const Navbar = async () => {
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
+
         <div className="flex items-center gap-5 text-black">
           {session && session?.user ? (
             <>
               <Link href="/startup/create">
                 <span>Create</span>
               </Link>
-              <button onClick={signOut}>
-                <span>Logout</span>
-              </button>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
               <Link href={`/user/$session?.id`}>
                 <span>{session?.user?.name}</span>
               </Link>
